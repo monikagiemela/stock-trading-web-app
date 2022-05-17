3dc5134d0e49d0400076534a01a4d4237e89ed5d
Monika Giemela <monikagiemela@gmail.com>
2022-05-17 11:35:55 +0200

    Cleaning before deployment

--- app/views/views_quote.py
+++ app/views/views_quote.py
@@ -8,7 +8,10 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import app, get_db_connection
-from app.helpers import apology, login_required, lookup, usd, absolute
+from app.helpers import apology, login_required, lookup, end_day_lookup, usd, absolute
+
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 @app.route("/quote", methods=["GET", "POST"])
@@ -33,4 +36,6 @@ def quote():
         quote = lookup(symbol)
         if not symbol or not quote:
             return apology("Enter a valid stock symbol", 400)
+        
+        #end_day_lookup(symbol)
         return render_template("quoted.html", quote=quote, symbol=symbol, user_credit=user_credit)
